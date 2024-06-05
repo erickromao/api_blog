16a7090613819a1b5378e5fdad53d65a22056758
erickromao <erickromao50@gmail.com>
2024-06-05 09:00:20 -0300

    listagem das postagem

--- src/controllers/PostsController.js
+++ src/controllers/PostsController.js
@@ -1,5 +1,6 @@
 const AppError = require('../utils/AppError')
 const knex = require('../database/knex')
+const validator = require('validator')
 
 class PostsController{
 
@@ -97,7 +98,174 @@ class PostsController{
         })
     }
 
-}
+    async read (request, response){
+        const {categoria, data} = request.body
+        
+        let posts, currentPosts
+
+        if(categoria && data){
+            if(!validator.isISO8601(data, {strict:true})){
+                throw new AppError('Modelo de data inválida. Use o formato YYYY-MM-DD.')
+            }
+            posts = await knex('posts')
+            .leftJoin('comments', "posts.id","comments.id_post")
+            .select([
+                'posts.*',
+                'comments.id as comment_id',
+                'comments.conteudo as comment_conteudo',
+                'comments.author as comment_author',
+                'comments.likes as comment_likes',
+                'comments.created_at as comment_created_at'
+            ])
+            .where('posts.created_at','>=',data)
+            .where('posts.categoria', categoria)           
+            .orderBy("posts.created_at")
+
+            if(posts.length ===0){
+                throw new AppError('Não existe posts apartir destes filtros: (data) (categoria).')
+            }
+
+            currentPosts = posts.map(post=>{
+                return {
+                    id: post.post_id,
+                    title: post.title,
+                    conteudo: post.conteudo,
+                    categoria: post.categoria,
+                    author: post.author,
+                    likes:post.likes,
+                    comments: post.comments,
+                    created_at: post.created_at,
+                    comment:{
+                        id:post.comment_id,
+                        conteudo: post.comment_conteudo,
+                        author: post.comment_author,
+                        likes: post.comment_likes,
+                        created_at: post.comment_created_at
+                    }
+                }
+            })
+        }
+
+        if(categoria && !data){
+            [posts] = await knex('posts')
+            .leftJoin('comments', "posts.id","comments.id_post")
+            .select([
+                'posts.*',
+                'comments.id as comment_id',
+                'comments.conteudo as comment_conteudo',
+                'comments.author as comment_author',
+                'comments.likes as comment_likes',
+                'comments.created_at as comment_created_at'
+            ])
+            .where('posts.categoria',categoria)            
+            .orderBy("posts.created_at")
+            
+            if(!posts){
+                throw new AppError(`Nenhum post com a categoria (${categoria}), foi encontrado.`)
+            }
+
+            currentPosts = [posts].map(post=>{
+                return {
+                    id: post.post_id,
+                    title: post.title,
+                    conteudo: post.conteudo,
+                    categoria: post.categoria,
+                    author: post.author,
+                    likes:post.likes,
+                    comments: post.comments,
+                    created_at: post.created_at,
+                    comment:{
+                        id:post.comment_id,
+                        conteudo: post.comment_conteudo,
+                        author: post.comment_author,
+                        likes: post.comment_likes,
+                        created_at: post.comment_created_at
+                    }
+                }
+            })
+        }
+
+        if(!categoria && data){
+            if(!validator.isISO8601(data, {strict:true})){
+                throw new AppError('Modelo de data inválida. Use o formato YYYY-MM-DD.')
+            }
+            posts = await knex('posts')
+            .leftJoin('comments', "posts.id","comments.id_post")
+            .select([
+                'posts.*',
+                'comments.id as comment_id',
+                'comments.conteudo as comment_conteudo',
+                'comments.author as comment_author',
+                'comments.likes as comment_likes',
+                'comments.created_at as comment_created_at'
+            ])
+            .where('posts.created_at','>=',data)            
+            .orderBy("posts.created_at")
+
+            if(posts.length ===0){
+                throw new AppError('Não existe posts apartir desta data.')
+            }
+
+            currentPosts = posts.map(post=>{
+                return {
+                    id: post.post_id,
+                    title: post.title,
+                    conteudo: post.conteudo,
+                    categoria: post.categoria,
+                    author: post.author,
+                    likes:post.likes,
+                    comments: post.comments,
+                    created_at: post.created_at,
+                    comment:{
+                        id:post.comment_id,
+                        conteudo: post.comment_conteudo,
+                        author: post.comment_author,
+                        likes: post.comment_likes,
+                        created_at: post.comment_created_at
+                    }
+                }
+            })
+        }
+
+        posts = await knex('posts')
+            .leftJoin('comments', "posts.id","comments.id_post")
+            .select([
+                'posts.*',
+                'comments.id as comment_id',
+                'comments.conteudo as comment_conteudo',
+                'comments.author as comment_author',
+                'comments.likes as comment_likes',
+                'comments.created_at as comment_created_at'
+            ])            
+            .orderBy("posts.created_at")
+
+            currentPosts = posts.map(post=>{
+                return {
+                    id: post.post_id,
+                    title: post.title,
+                    conteudo: post.conteudo,
+                    categoria: post.categoria,
+                    author: post.author,
+                    likes:post.likes,
+                    comments: post.comments,
+                    created_at: post.created_at,
+                    comment:{
+                        id:post.comment_id,
+                        conteudo: post.comment_conteudo,
+                        author: post.comment_author,
+                        likes: post.comment_likes,
+                        created_at: post.comment_created_at
+                    }
+                }
+            })
+
+        return response.json({
+            message:"Postagens encontradas:",
+            posts:currentPosts
+        })
+    }
+   
+}   
 
 
 module.exports = PostsController
--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -12,6 +12,7 @@ postsRouter.post("/",authentication, postsController.create)
 postsRouter.delete("/:id", authentication, postsController.delete)
 postsRouter.put("/:id", authentication, postsController.update)
 postsRouter.post("/:id/comment", authentication, commentsController.create)
+postsRouter.get("/", authentication, postsController.read)
 
 module.exports = postsRouter
 
